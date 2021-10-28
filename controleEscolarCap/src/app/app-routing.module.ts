337dbde3396116ad4e2fce65bc4c42e48323658b
AnaMelo <analuisa.tomita@hotmail.com>
2021-10-28 08:44:09 -0300

    criado paginas cadastro disciplina e listar disciplina

--- controleEscolarCap/src/app/app-routing.module.ts
+++ controleEscolarCap/src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { MatriculaListarComponent } from './matricula-listar/matricula-listar.co
 import { NotasListarAlunoComponent } from './notas-listar-aluno/notas-listar-aluno.component';
 import { NotasListarTurmaComponent } from './notas-listar-turma/notas-listar-turma.component';
 import { ProfessorCadastrarComponent } from './professor-cadastrar/professor-cadastrar.component';
+import { DisciplinaCadastrarComponent } from './disciplina-cadastrar/disciplina-cadastrar.component';
+import { DisciplinaListarComponent } from './disciplina-listar/disciplina-listar.component';
 
 const routes: Routes = [
   {path: 'professor-cadastrar', component: ProfessorCadastrarComponent},
   {path: 'matricula-cadastrar', component: MatriculaCadastrarComponent},
   {path: 'matricula-listar', component: MatriculaListarComponent},
   {path: 'notas-listar-turma', component: NotasListarTurmaComponent},
-  {path: 'notas-listar-aluno', component: NotasListarAlunoComponent}
+  {path: 'notas-listar-aluno', component: NotasListarAlunoComponent},
+  {path: 'disciplina-cadastrar', component: DisciplinaCadastrarComponent},
+  {path: 'disciplina-listar', component: DisciplinaListarComponent}
 ];
 
 @NgModule({
