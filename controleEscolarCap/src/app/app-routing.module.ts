bbc862c943e6947dc3a248c25e3b77d7b4809f0b
anaRita7 <ana.cruz@ee.ufcg.edu.br>
2021-10-27 23:20:26 -0300

    anar - tela de consulta de notas por aluno

--- controleEscolarCap/src/app/app-routing.module.ts
+++ controleEscolarCap/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MatriculaCadastrarComponent } from './matricula-cadastrar/matricula-cadastrar.component';
 import { MatriculaListarComponent } from './matricula-listar/matricula-listar.component';
+import { NotasListarAlunoComponent } from './notas-listar-aluno/notas-listar-aluno.component';
 import { NotasListarTurmaComponent } from './notas-listar-turma/notas-listar-turma.component';
 import { ProfessorCadastrarComponent } from './professor-cadastrar/professor-cadastrar.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path: 'professor-cadastrar', component: ProfessorCadastrarComponent},
   {path: 'matricula-cadastrar', component: MatriculaCadastrarComponent},
   {path: 'matricula-listar', component: MatriculaListarComponent},
-  {path: 'notas-listar-turma', component: NotasListarTurmaComponent}
+  {path: 'notas-listar-turma', component: NotasListarTurmaComponent},
+  {path: 'notas-listar-aluno', component: NotasListarAlunoComponent}
 ];
 
 @NgModule({
